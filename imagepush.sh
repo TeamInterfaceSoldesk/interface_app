e98ac9a8e9eb98293719d9ded55ac80577f22c11
markshyou <markshyou90@gmail.com>
2023-07-19 14:04:45 +0900

    수정
    수정

--- imagepush.sh
+++ imagepush.sh
@@ -2,14 +2,19 @@
 
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com
 
-docker tag order_docker:latest 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:latest
+docker tag order-docker:latest 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:latest
 
-docker tag store_docker:latest 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:latest
+docker tag store-docker:latest 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:latest
 
-docker tag user_docker:latest 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:latest
+docker tag user-docker:latest 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:latest
 
-docker tag frontend_docker:latest 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:latest
+docker tag frontend-docker:latest 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:latest
 
-docker tag traefik:latest 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:latest
+docker push 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:order-docker
 
-docker tag traefik/whoami:latest 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:latest
+docker push 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:store-docker
+
+docker push 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:user-docker
+
+docker push 480936841767.dkr.ecr.ap-northeast-2.amazonaws.com/interface_app:frontend-docker
+
--- make_all.sh
+++ make_all.sh
@@ -17,7 +17,3 @@ make
 # user 디렉터리로 이동하여 make 실행
 cd ~/environment/interface_app/user
 make
-
-docker build -t traefik
-
-docker build -t traefik/whoami
